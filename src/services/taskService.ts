aa82dbdb89471c0b9e9c921836084969bffc54c2
Justxt <jostinmatt@gmail.com>
2025-04-21 14:11:10 -0500

    Se configura para subirlo a vercel

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,19 +1,20 @@
 import axios from "axios";
 import { Task, TaskFormData, TaskFilterData } from "../types/task";
-
-const API_BASE_URL = "http://localhost:3000";
-
-const apiClient = axios.create({
-  baseURL: `${API_BASE_URL}/tasks`,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+import { API_URL } from "../config/env";
 
 export const getTasks = async (filters?: TaskFilterData): Promise<Task[]> => {
   try {
-    // Construye los parámetros de consulta directamente
-    const response = await apiClient.get("/", { params: filters }); // Usa la instancia apiClient
+    const params = new URLSearchParams();
+
+    if (filters) {
+      if (filters.startDate) params.append("startDate", filters.startDate);
+      if (filters.endDate) params.append("endDate", filters.endDate);
+      if (filters.delayed) params.append("delayed", String(filters.delayed));
+    }
+
+    const response = await axios.get(
+      `${API_URL}${params.toString() ? `?${params.toString()}` : ""}`
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching tasks:", error);
@@ -23,7 +24,7 @@ export const getTasks = async (filters?: TaskFilterData): Promise<Task[]> => {
 
 export const getTask = async (id: number): Promise<Task | null> => {
   try {
-    const response = await apiClient.get(`/${id}`); // Usa la instancia apiClient
+    const response = await axios.get(`${API_URL}/${id}`);
     return response.data;
   } catch (error) {
     console.error(`Error fetching task ${id}:`, error);
@@ -33,7 +34,7 @@ export const getTask = async (id: number): Promise<Task | null> => {
 
 export const createTask = async (task: TaskFormData): Promise<Task | null> => {
   try {
-    const response = await apiClient.post("/", task); // Usa la instancia apiClient
+    const response = await axios.post(API_URL, task);
     return response.data;
   } catch (error) {
     console.error("Error creating task:", error);
@@ -46,7 +47,7 @@ export const updateTask = async (
   task: Partial<TaskFormData>
 ): Promise<Task | null> => {
   try {
-    const response = await apiClient.patch(`/${id}`, task); // Usa la instancia apiClient
+    const response = await axios.patch(`${API_URL}/${id}`, task);
     return response.data;
   } catch (error) {
     console.error(`Error updating task ${id}:`, error);
@@ -56,7 +57,7 @@ export const updateTask = async (
 
 export const deleteTask = async (id: number): Promise<boolean> => {
   try {
-    await apiClient.delete(`/${id}`); // Usa la instancia apiClient
+    await axios.delete(`${API_URL}/${id}`);
     return true;
   } catch (error) {
     console.error(`Error deleting task ${id}:`, error);
